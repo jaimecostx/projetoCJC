9a82067cd0778a657b8e3af4834b312e16b14eb9
Jaime Costa <tmdcosta.17@gmail.com>
2023-05-18 21:36:54 +0100

    IntroMenu looking good

--- ProjetoCjC/Assets/Karting/Scripts/Custom/HatModel.cs
+++ ProjetoCjC/Assets/Karting/Scripts/Custom/HatModel.cs
@@ -9,14 +9,25 @@ namespace Game{
     {
         public GameObject[] hatPrefabs;
         public int currentHatIndex = 0;
-        public GameObject headEnd;
+        public GameObject classicHeadEND;
+        public GameObject roadsterHeadEND;
+        GameObject headEND;
         GameObject hatInstance;
-        GameObject carObject;
-        CarModel car;
-        // Start is called before the first frame update
+        
+        /// <summary>
+        /// The Start method is called when the script instance is being initialized.
+        /// It retrieves the value of the "CarModel" string from the PlayerPrefs and assigns the appropriate headEND GameObject.
+        /// </summary>
         void Start()
         {
-            carObject = GameObject.Find("KartClassic_Player");
+            if (PlayerPrefs.GetString("CarModel") == "0")
+            {
+                headEND = classicHeadEND;
+            }
+            else if (PlayerPrefs.GetString("CarModel") == "1")
+            {
+                headEND = roadsterHeadEND;
+            }
         }
 
         // Update is called once per frame
@@ -25,42 +36,41 @@ namespace Game{
         
         }
 
-    public void OnButtonClick()
-    {
-        if (PlayerPrefs.GetString("CarModel") == "1")
+        public void OnButtonClick()
         {
-            carObject = GameObject.Find("Roadster_Player");
-            headEnd = carObject.transform.Find("KartVisual/PlayerIdle/Root1/Hips/Spine1/Spine2/Neck/Head/HeadEND");
+            if (PlayerPrefs.GetString("CarModel") == "0")
+            {
+                headEND = classicHeadEND;
+            }
+            else if (PlayerPrefs.GetString("CarModel") == "1")
+            {
+                headEND = roadsterHeadEND;
+            }
+
+            hatCycle();
         }
-        else
+
+        /// <summary>
+        /// hatCycle method handles the logic for cycling through hatPrefabs and instantiating the selected hat.
+        /// It also manages destroying the previous hat instance and updating PlayerPrefs with the current hat index.
+        /// </summary>
+        void hatCycle ()
         {
-            carObject = GameObject.Find("KartClassic_Player");
-            headEnd = carObject.transform.Find("KartBody/PlayerIdle/Root1/Hips/Spine1/Spine2/Neck/Head/HeadEND");
+            if (hatInstance != null)
+            {
+                Destroy(hatInstance);
+                hatInstance = null;
+            }
+            currentHatIndex = (currentHatIndex + 1) % (hatPrefabs.Length+1);
+
+            if (currentHatIndex != hatPrefabs.Length){
+                hatInstance = Instantiate(hatPrefabs[currentHatIndex]);
+                hatInstance.transform.position = headEND.transform.position;
+                hatInstance.transform.rotation = headEND.transform.rotation;
+                hatInstance.transform.localScale = new Vector3(2f, 2f, 2f);
+                hatInstance.transform.SetParent(headEND.transform);
+            }
+            PlayerPrefs.SetInt("HatModel", currentHatIndex);
         }
-        
-        hatCycle();
     }
-
-    void hatCycle ()
-    {
-        
-        if (hatInstance != null)
-        {
-            Destroy(hatInstance);
-            hatInstance = null;
-        }
-
-        currentHatIndex = (currentHatIndex + 1) % (hatPrefabs.Length+1);
-
-        if (currentHatIndex != hatPrefabs.Length){
-            hatInstance = Instantiate(hatPrefabs[currentHatIndex]);
-            hatInstance.transform.position = headEnd.transform.position;
-            hatInstance.transform.rotation = headEnd.transform.rotation;
-            hatInstance.transform.localScale = new Vector3(2f, 2f, 2f);
-            hatInstance.transform.SetParent(headEnd.transform);
-        }
-        PlayerPrefs.SetInt("HatModel", currentHatIndex);
-    }
-}
-
 }
